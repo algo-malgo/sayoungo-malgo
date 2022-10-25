b58175c98408b6e65af165e18967b8ac959df7e0
ChoiSeYoung <sychoi0516@naver.com>
2022-10-25 16:55:22 +0900

    최종 코드

---
+++ programmers/Lv.2/올바른 괄호/main.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") stack.push(s[i]);
+    else {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+  if (stack.length !== 0) return false;
+  return true;
+}
