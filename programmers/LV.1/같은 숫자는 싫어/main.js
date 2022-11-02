09147d0d05b44694665d746e75328abd9d72f1f1
ChoiSeYoung <sychoi0516@naver.com>
2022-11-02 16:44:10 +0900

    최종 코드

---
+++ programmers/LV.1/같은 숫자는 싫어/main.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  const answer = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (answer[answer.length - 1] !== arr[i]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
