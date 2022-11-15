7277dcc6a1ad27376b65ecaa4d0002b0b85c8268
ChoiSeYoung <sychoi0516@naver.com>
2022-11-16 01:37:41 +0900

    필요없는 코드 삭제

--- programmers/Lv.2/카펫/main.js
+++ programmers/Lv.2/카펫/main.js
@@ -1,9 +1,7 @@
 function solution(brown, yellow) {
-  let width = 0;
   let height = 0;
   for (let i = Math.ceil(Math.sqrt(yellow)); i <= yellow; i++) {
     if (yellow % i !== 0) continue;
-    width = i;
     height = yellow / i;
 
     if (brown === (i + 2) * 2 + height * 2) return [i + 2, height + 2];
