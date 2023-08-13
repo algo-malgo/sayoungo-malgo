27f1ae725ad62b0e01d9eef1506a04d43e8f2a4c
seyoung choi <sychoi0516@naver.com>
2023-08-13 15:17:47 +0900

    최종 코드

---
+++ programmers/PCCP/1회/1번/main.js
@@ -0,0 +1,16 @@
+function solution(input_string) {
+  const count = new Array(26).fill(0);
+  let answer = "";
+  for (let i = 0; i < input_string.length; i++) {
+    if (input_string[i] === input_string[i + 1]) continue;
+    else {
+      if (count[input_string.charCodeAt(i) - 97] > 0) {
+        answer += input_string[i];
+      }
+      count[input_string.charCodeAt(i) - 97]++;
+    }
+  }
+  if (answer.length === 0) answer = "N";
+  else answer = [...new Set(answer.split(""))].sort().join("");
+  return answer;
+}
